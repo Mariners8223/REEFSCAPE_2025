33e6837f9b8109b253ea291a8032f2121b4584a4
unknown <ironih.frc@gmail.com>
2025-09-30 19:24:39 +0300

    uykf

--- src/main/java/frc/robot/subsystems/Climb/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/Climb/ClimbConstants.java
@@ -10,8 +10,10 @@ public class ClimbConstants {
     public static final double GEAR_RATIO = 15;
     public static final double ROTATIONS_TO_METERS = 0.024 * 2 * Math.PI;
     
-    public static final double CLIMB_POWER = -0.65;
-    public static final double SOFT_MINIMUM = -9.5 * 0.78;
+    public static final double CLIMB_POWER = -1;//-0.65
+    public static final double SOFT_MINIMUM = -9.5 * 1; //-9.5 * 0.78
+
+
     public static final double SOFT_MAXIMUM = 0;
     public static final boolean IS_INVERTED = true;
     public static final double START_POSITION = 0.125;
